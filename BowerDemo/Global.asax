497bd0db188db0a3424b6cfdf9214fd57a78633e
Fourdot08 <Fourdot08@prognosys.com>
2016-06-28 13:31:14 +0530

    Added bower project

---
+++ BowerDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BowerDemo.MvcApplication" Language="C#" %>
---
+++ BowerDemo/gulpfile.js
@@ -0,0 +1,17 @@
+﻿var gulp = require('gulp');
+var clean = require('gulp-clean');
+
+//Clean the lib directory
+gulp.task("clean", function () {
+    gulp.src('Lib/*', { read: false }).pipe(clean({ force: true }));
+});
+
+//Copy the minified scripts from the bower_components directory to the lib directory
+gulp.task('copyScripts', function () {
+    gulp.src(['bower_components/jquery/dist/jquery.min.js'])
+        .pipe(gulp.dest('Lib/Js'));
+});
+
+
+//Running multiple tasks in series
+gulp.task('copy', ['clean', 'copyScripts']);
